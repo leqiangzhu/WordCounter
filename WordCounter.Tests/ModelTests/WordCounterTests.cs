b8de5759bca82939a1787c0cf3b0e9118a35c088
David Zhu <zhuleqiang@gmail.com>
2018-09-07 12:35:28 -0700

    add the test file,but without the test method

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WordCounter.Models;
+using System;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class RepeatCounterTest
+  {
+    [TestMethod]
+    public void GetWord_ReturnUserWord_String()
+    {
+      //Arrange
+
+      //Act
+
+      //Assert
+
+    }
+
+    }
+
+  }
